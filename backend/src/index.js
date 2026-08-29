a753fdb44d46a2e6612e9c20d6759eeb6b5e5f05
rishith padala <rishithpadala@rishiths-MacBook-Air.local>
2026-08-29 20:08:04 +0530

    Add monolithic Docker deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,7 +32,9 @@ if (fs.existsSync(publicDir)) {
 
 app.listen(PORT,()=>{
     connectDB();
-    console.log("running on port 3000")
+    console.log("running on port 3000");
+    if(process.env.NODE_ENV==="production")
+    job.start();
 });
 
 // node latest version  . no need to nodemon  wecan use --watch in script . so it autmaically restarts the index.js where there are any kind of changes 
