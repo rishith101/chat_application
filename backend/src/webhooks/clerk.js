d01fd310a74d63b7e2f835c0ab2585b53d0d1aac
rishith padala <rishithpadala@rishiths-MacBook-Air.local>
2026-08-29 22:12:00 +0530

    Add monolithic Docker deployment

--- backend/src/webhooks/clerk.js
+++ backend/src/webhooks/clerk.js
@@ -27,7 +27,7 @@ router.post("/",async (req,res) =>{
 
             await user.findOneAndUpdate({clerkId:u.id},
                 {clerkId:u.id,email,fullName,profilePic:u.image_url},
-                { new:true ,upsert:true ,setDefaultOnInsert:true},
+                { new:true ,upsert:true ,setDefaultsOnInsert:true},
             );
         }
 
